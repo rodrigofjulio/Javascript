9685a4de518b83fec5fb49da075362559d580f71
Rodrigo Franklin Júlio <56088459+rodrigofjulio@users.noreply.github.com>
2022-08-12 20:52:25 -0300

    Add files via upload

---
+++ atividade_teste.js
@@ -0,0 +1,24 @@
+var salario = 2000.00;
+var aumento = 0.15;
+var inflacao = 0.07;
+
+if(salario >= 2000.00){// Se o salario for igual ou acima de R$ 2000.00, vai receber uma inflação de 10%
+    var inflacao = inflacao(x);
+}else{
+    var novoSalarioInflacao = salario - (inflacao * novoSalario);
+}
+
+function inflacao(x){
+    return 0.10;
+}
+
+//var novoSalario = salario + (aumento * salario);
+//no periodo a inflação foi de 7%. Faça o cálculo abaixo para verificar a perda;
+
+//var novoSalarioInflacao = novoSalario - (inflacao * novoSalario);
+//apresente a saída com console log para "novoSalario" "novoSalarioInflacao";
+
+console.log(novoSalario);
+
+console.log(novoSalarioInflacao);
+
